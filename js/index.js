5f2cc5f0975ba6f91ec155b06a6a95f3328a904a
mobiusdonut <modularmercury@gmail.com>
2020-05-10 16:35:03 -0700

    index.html links to subpages now

--- js/index.js
+++ js/index.js
@@ -6,4 +6,5 @@ const info = (pfp) => {
     document.getElementById("infoimg").src = "img/" + pfp + "info.png"
     document.getElementById("infotitle").innerHTML = pfp;
     document.getElementById("infop").innerHTML = data[pfp];
+    document.getElementById("infolink").href = pfp;
 }
